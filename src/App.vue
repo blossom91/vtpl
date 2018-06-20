9572d379b04ed52d439c3ddba9a48cb874611413
zhao.hang <zhao.hang@uneed.com>
2018-06-20 09:59:29 +0800

    feat: 我的zh分支

--- src/App.vue
+++ src/App.vue
@@ -16,25 +16,33 @@
             <header class="content-hd">
                 <section class="title">
                     <div class="name">
-                        <h1>郭梦莉</h1>
-                        <h3 class='head-job'>招采工程师</h3>
+                        <h1>赵航</h1>
+                        <h3 class='head-job'>前端工程师</h3>
                     </div>
                     <div class="job">
-                        <img src="./guo.jpeg" style='width:120px' alt="">
+
                     </div>
                 </section>
                 <section class="info">
                     <ul>
-                        <li>女 / 1991.04</li>
-                        <li>西安工业大学 · 结构工程</li>
-                        <li>硕士 / 2017年毕业</li>
+                        <li>男 / 1991.02</li>
+                        <li>河南大学</li>
+                        <li>本科 / 2014年毕业</li>
                     </ul>
                 </section>
                 <section class="contact">
                     <ul>
                         <li>
+                            <a href="http://www.zhaohang.site/" target="_blank">
+                                <span class="contact-link">我的博客</span>
+                                <span class="iconfont">
+                                    <img style="width:16px;height:16px;" src="./博客.png" alt="">
+                                </span>
+                            </a>
+                        </li>
+                        <li>
                             <a href="mailto:252326977@qq.com">
-                                <span class="contact-link">252326977@qq.com</span>
+                                <span class="contact-link">blossom@zhfei.cc</span>
                                 <span class="iconfont">
                                     <svg width="16px" height="16px" viewBox="0 0 1024 1024">
                                         <path d="M903.808 239.04l-818.304 0 0 545.92 852.992 0 0-545.92-34.688 0zM869.664 273.152l-329.536 329.536c-15.04 15.04-41.248 15.04-56.288 0l-329.6-329.536 715.392 0zM119.616 286.752l221.536 221.504-221.536 221.536 0-443.072zM146.848 750.848l218.464-218.432 94.432 94.4c13.952 13.952 32.512 21.664 52.256 21.664s38.304-7.712 52.256-21.664l94.432-94.432 218.432 218.432-730.304 0zM904.384 729.824l-221.536-221.568 221.536-221.568 0 443.136z"></path>
@@ -44,7 +52,7 @@
                         </li>
                         <li>
                             <a href="tel:18513121736">
-                                <span class="contact-link">18513121736</span>
+                                <span class="contact-link">18513119219</span>
                                 <span class="iconfont">
                                     <svg width="16px" height="16px" viewBox="0 0 1024 1024">
                                         <path d="M256 1024l512 0c26.464 0 48-21.536 48-48l0-928c0-26.464-21.536-48-48-48l-512 0c-26.464 0-48 21.536-48 48l0 928c0 26.464 21.536 48 48 48zM240 48c0-8.832 7.168-16 16-16l512 0c8.832 0 16 7.168 16 16l0 928c0 8.832-7.168 16-16 16l-512 0c-8.832 0-16-7.168-16-16l0-928zM288 864l448 0c8.832 0 16-7.168 16-16l0-672c0-8.832-7.168-16-16-16l-448 0c-8.832 0-16 7.168-16 16l0 672c0 8.832 7.168 16 16 16zM304 192l416 0 0 640-416 0 0-640zM496 928c0 17.664 14.336 32 32 32s32-14.336 32-32c0-17.664-14.336-32-32-32-17.664 0-32 14.336-32 32zM448 128l128 0c8.832 0 16-7.168 16-16s-7.168-16-16-16l-128 0c-8.832 0-16 7.168-16 16s7.168 16 16 16z"></path>
@@ -87,23 +95,15 @@
                         <div class="section-bd">
                             <div class="item">
                                 <header class="item-hd">
-                                    <h3 class="item-name">天职工程咨询股份有限公司 (2017.07 至今)</h3>
+                                    <h3 class="item-name">北京与你科技有限工程 (2016.04 至今)</h3>
                                     <!-- <span class="item-time">2017.07 至今</span> -->
-                                    <span class="item-time">管理审计</span>
                                 </header>
-                                <div class="item-bd">
-                                    主要从事工程管理审计、招采专项检查、项目群检查、项目后评价等工作。
-                                </div>
-                            </div>
-                            <div class="item">
-                                <header class="item-hd">
-                                    <h3 class="item-name">河南普华建筑设计院（2016.09 - 2017.04 实习）</h3>
-                                    <!-- <span class="item-time">2016.09 - 2017.04</span> -->
-                                    <span class="item-time">结构工程师助理</span>
-                                </header>
-                                <div class="item-bd">
-                                    主要从事建筑结构施工图纸的绘制与修订，解决施工方的技术疑问，参与甲方需求研讨会等。
-                                </div>
+                                <ul class="item-bd">
+                                    <li>日常使用vue全家桶，webpack构建了一套常用的前端工作流。</li>
+                                    <li>使用npm管理常用工具库代码，主要用于hybrid开发。</li>
+                                    <li>偶尔写点后端语言，node多用于爬虫和小工具，项目中使用go</li>
+                                    <li>对技术充满热情，根据工作需要随时学习相应技术。</li>
+                                </ul>
                             </div>
                         </div>
                     </section>
@@ -116,181 +116,34 @@
                         <div class="section-bd">
                             <div class="item">
                                 <header class="item-hd">
-                                    <span>北汽福田汽车股份有限公司建设项目工程管理及效益审计</span>
+                                    <span>公司后台管理项目</span>
                                 </header>
-                                <div class="item-bd">
-                                    <ul>
-                                        <li>担任该项目检查人员，负责招标采购检查等。</li>
-                                    </ul>
-                                </div>
+                                <ul class="item-bd">
+                                    <li>前端使用vue全家桶,iview UI框架,webpack构建</li>
+                                    <li>后端使用go,简单使用了mysql与redis数据库</li>
+                                    <li>实现了简单的聊天功能,oss上传控件,用户资料管理功能,注册用户数据统计</li>
+                                    <li>最主要实现了一套服务端用于部署发布的前端控制平台,包括项目自动发布,自动重启,选择回滚,发布记录保存等</li>
+                                    <li>也实现了前端hybrid热更新的打包发布功能</li>
+                                </ul>
+                                <img style='width:100%' src="./1.png" alt="">
                             </div>
                             <div class="item">
                                 <header class="item-hd">
-                                    <span>华夏幸福基业2017年项目营销及规划类招采管理检查</span>
+                                    <a href="https://photos.uneed.com/">相册管理系统(对外)</a>
                                 </header>
-                                <div class="item-bd">
-                                    <ul>
-                                        <li>代表华夏幸福基业总部对各大区的采购项目进行招采检查，主要检查招标采购的合规性、合理性及管控体系的有效性。</li>
-                                        <li>担任该项目检查人员，负责招标采购检查等。</li>
-                                    </ul>
-                                </div>
-                            </div>
-
-                            <div class="item">
-                                <header class="item-hd">
-                                    <span>北京粮食集团有限责任公司2017年度基建项目工程管理咨询-项目群检查</span>
-                                </header>
-                                <div class="item-bd">
-                                    <ul>
-                                        <li>对北京粮食集团各项目公司2016-2017年度发生的项目从内部决策、项目立项、勘察设计、招标采购、合同管理、施工管理、竣工验收、档案管理八个方面进行检查。</li>
-                                        <li>担任该项目的检查人员，对项目的全过程进行审计。</li>
-                                    </ul>
-                                </div>
+                                <ul class="item-bd">
+                                    <li>前端使用vue全家桶,为了提高用户体验,大量使用了vuex状态管理,对外项目ui自写</li>
+                                    <li>项目地址(https://photos.uneed.com/)</li>
+                                </ul>
                             </div>
                             <div class="item">
                                 <header class="item-hd">
-                                    <span>中粮置业有限公司中粮安定门项目全过程跟踪审计</span>
+                                    <span>客户端内部页面</span>
                                 </header>
-                                <div class="item-bd">
-                                    <ul>
-                                        <li>对中粮置业有限公司安定门部分项目进行招采检查，主要检查项目招标采购的合规性与合理性。</li>
-                                        <li>参与本项目招采部分检查，对项目采购的全过程进行审计。</li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="item">
-                                <header class="item-hd">
-                                    <span>华夏幸福基业股份有限公司华夏幸福2018成本招采第三方检查咨询服务</span>
-                                </header>
-                                <div class="item-bd">
-                                    <ul>
-                                        <li>华夏幸福招采季度检查，主要检查项目招标采购的合规性与合理性。</li>
-                                        <li>担任该项目检查人员，负责招标采购检查等。</li>
-                                    </ul>
-                                </div>
-                            </div>
-
-                            <!-- <div class="item">
-                                <header class="item-hd">
-                                    <a class="btn item-more" href="https://golang.org">Golang</a>
-                                </header>
-                                <div class="item-bd">
-                                    <ul>
-                                        <li>
-                                            纯go写的直播服务端
-                                        </li>
-                                        <li>
-                                            简约的HTTP框架
-                                        </li>
-                                        <li>
-                                            高性能轻量级socks5网络混淆代理，可翻越GFW
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div> -->
-                        </div>
-                    </section>
-
-                </div>
-
-                <div class="content-right">
-                    <section class="skill">
-
-                        <!-- <header class="section-hd"> -->
-                        <!-- <span class="section-title-l"></span> -->
-                        <!-- <h2 class="section-title"></h2> -->
-                        <!-- <span class="section-title-r"></span> -->
-                        <!-- </header> -->
-                        <div class="section-bd">
-                            <div class="item">
-                                <header class="item-hd">
-                                    <span>中国印钞造币总公司基建项目全过程管理审计</span>
-                                </header>
-                                <div class="item-bd">
-                                    <ul>
-                                        <li>对昆山钞票纸业有限公司与上海造币公司基建项目进行全过程跟踪审计。</li>
-                                        <li>担任该项目检查人员，负责招标采购检查等。</li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="item">
-                                <header class="item-hd">
-                                    <span>远洋地产控股有限公司2018年度供方库调研咨询服务</span>
-                                </header>
-                                <div class="item-bd">
-                                    <ul>
-                                        <li>对远洋供方库进行梳理与关联关系的检查，并对供应商类别进行重新定义与划分。</li>
-                                        <li>担任该项目检查人员。</li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="item">
-                                <header class="item-hd">
-                                    <span>中粮广场改造项目招采专项检查</span>
-                                </header>
-                                <div class="item-bd">
-                                    <ul>
-                                        <li>对中粮广场改造项目进行招采检查，主要检查项目招标采购的合规性与合理性。</li>
-                                        <li>参与本项目招采部分检查，对项目采购的全过程进行审计。</li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="item">
-                                <header class="item-hd">
-                                    <span>阿里巴巴基建项目管理审计</span>
-                                </header>
-                                <div class="item-bd">
-                                    <ul>
-                                        <li>对阿里巴巴的基建项目进行招采检查，主要检查项目招标采购的合理性、合规性。</li>
-                                        <li>担任本项目检查人员。</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-
-                    <section class="skill">
-                        <header class="section-hd">
-                            <span class="section-title-l"></span>
-                            <h2 class="section-title">业务能力</h2>
-                            <span class="section-title-r"></span>
-                        </header>
-                        <div class="section-bd">
-
-                            <div class="item">
-                                <div class="item-bd">
-                                    <header class="item-hd">
-                                        <!-- <span class="item-time">业务能力</span> -->
-                                    </header>
-                                    <ul class="section-content">
-                                        <li>熟练掌握office办公软件。</li>
-                                        <li>熟悉房地产开发项目的建设程序、管理重点。</li>
-                                        <li>具备多项目管理审计实施经验，熟悉国家的相关法律法规。</li>
-                                        <li>对工程建设整个过程中的招投标、工程建设管理、质量管理等具有丰富的管理和实践经验。</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                    <section class="skill">
-                        <header class="section-hd">
-                            <span class="section-title-l"></span>
-                            <h2 class="section-title">自我评价</h2>
-                            <span class="section-title-r"></span>
-                        </header>
-                        <div class="section-bd">
-
-                            <div class="item">
-                                <div class="item-bd">
-                                    <header class="item-hd">
-                                        <!-- <span class="item-time">业务能力</span> -->
-                                    </header>
-                                    <ul class="section-content">
-                                        <li>对工程管理行业有浓厚兴趣，关注法律法规动态。</li>
-                                        <li>工作态度认真负责，具有团队合作精神。</li>
-                                        <li>学习能力强，逻辑清晰。</li>
-                                    </ul>
-                                </div>
+                                <ul class="item-bd">
+                                    <li>与你客户端内部页面包含: 钱包模块,红包模块,游戏模块,幻灯片和各种活动页面</li>
+                                    <li>欢迎下载app体验(*^__^*) 嘻嘻……</li>
+                                </ul>
                             </div>
                         </div>
                     </section>
