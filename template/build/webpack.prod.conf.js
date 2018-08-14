05c6effb7efcd0807f5042d970b2e07b3d6cd0df
zhao.hang <zhao.hang@uneed.com>
2018-08-14 15:15:55 +0800

    feat: 添加copy

--- template/build/webpack.prod.conf.js
+++ template/build/webpack.prod.conf.js
@@ -6,6 +6,7 @@ const webpack = require('webpack')
 const config = require('./config')
 const merge = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.conf')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
@@ -143,6 +144,14 @@ const webpackConfig = merge(baseWebpackConfig, {
         new webpack.optimize.ModuleConcatenationPlugin(),
 
         ...commonsChunkPlugins(),
+
+        new CopyWebpackPlugin([
+            {
+                from: path.resolve(__dirname, '../static'),
+                to: config.build.assetsSubDirectory,
+                ignore: ['.*'],
+            },
+        ]),
     ],
 })
 
