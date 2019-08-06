c1f4f1d7aecf67519e888e2ab422fad5ecea4f9c
zhao.hang <zhao.hang@uneed.com>
2019-08-06 23:31:27 +0800

    z

--- src/App.vue
+++ src/App.vue
@@ -11,17 +11,15 @@
         <a class="download-pdf" href="resume.pdf">下载 PDF</a>
 
         <main class="content">
-
             <!--基本信息-->
             <header class="content-hd">
                 <section class="title">
                     <div class="name">
                         <h1>郭梦莉</h1>
-                        <h3 class='head-job'>工程审计岗</h3>
-                        <h3 class='head-job'>招标采购岗</h3>
+                        <h3 class="head-job">招采专员</h3>
                     </div>
                     <div class="job">
-                        <img src="./guo.jpeg" style='width:120px' alt="">
+                        <img src="./guo.jpeg" style="width:120px" alt="" />
                     </div>
                 </section>
                 <section class="info">
@@ -38,7 +36,9 @@
                                 <span class="contact-link">252326977@qq.com</span>
                                 <span class="iconfont">
                                     <svg width="16px" height="16px" viewBox="0 0 1024 1024">
-                                        <path d="M903.808 239.04l-818.304 0 0 545.92 852.992 0 0-545.92-34.688 0zM869.664 273.152l-329.536 329.536c-15.04 15.04-41.248 15.04-56.288 0l-329.6-329.536 715.392 0zM119.616 286.752l221.536 221.504-221.536 221.536 0-443.072zM146.848 750.848l218.464-218.432 94.432 94.4c13.952 13.952 32.512 21.664 52.256 21.664s38.304-7.712 52.256-21.664l94.432-94.432 218.432 218.432-730.304 0zM904.384 729.824l-221.536-221.568 221.536-221.568 0 443.136z"></path>
+                                        <path
+                                            d="M903.808 239.04l-818.304 0 0 545.92 852.992 0 0-545.92-34.688 0zM869.664 273.152l-329.536 329.536c-15.04 15.04-41.248 15.04-56.288 0l-329.6-329.536 715.392 0zM119.616 286.752l221.536 221.504-221.536 221.536 0-443.072zM146.848 750.848l218.464-218.432 94.432 94.4c13.952 13.952 32.512 21.664 52.256 21.664s38.304-7.712 52.256-21.664l94.432-94.432 218.432 218.432-730.304 0zM904.384 729.824l-221.536-221.568 221.536-221.568 0 443.136z"
+                                        ></path>
                                     </svg>
                                 </span>
                             </a>
@@ -48,7 +48,9 @@
                                 <span class="contact-link">18513121736</span>
                                 <span class="iconfont">
                                     <svg width="16px" height="16px" viewBox="0 0 1024 1024">
-                                        <path d="M256 1024l512 0c26.464 0 48-21.536 48-48l0-928c0-26.464-21.536-48-48-48l-512 0c-26.464 0-48 21.536-48 48l0 928c0 26.464 21.536 48 48 48zM240 48c0-8.832 7.168-16 16-16l512 0c8.832 0 16 7.168 16 16l0 928c0 8.832-7.168 16-16 16l-512 0c-8.832 0-16-7.168-16-16l0-928zM288 864l448 0c8.832 0 16-7.168 16-16l0-672c0-8.832-7.168-16-16-16l-448 0c-8.832 0-16 7.168-16 16l0 672c0 8.832 7.168 16 16 16zM304 192l416 0 0 640-416 0 0-640zM496 928c0 17.664 14.336 32 32 32s32-14.336 32-32c0-17.664-14.336-32-32-32-17.664 0-32 14.336-32 32zM448 128l128 0c8.832 0 16-7.168 16-16s-7.168-16-16-16l-128 0c-8.832 0-16 7.168-16 16s7.168 16 16 16z"></path>
+                                        <path
+                                            d="M256 1024l512 0c26.464 0 48-21.536 48-48l0-928c0-26.464-21.536-48-48-48l-512 0c-26.464 0-48 21.536-48 48l0 928c0 26.464 21.536 48 48 48zM240 48c0-8.832 7.168-16 16-16l512 0c8.832 0 16 7.168 16 16l0 928c0 8.832-7.168 16-16 16l-512 0c-8.832 0-16-7.168-16-16l0-928zM288 864l448 0c8.832 0 16-7.168 16-16l0-672c0-8.832-7.168-16-16-16l-448 0c-8.832 0-16 7.168-16 16l0 672c0 8.832 7.168 16 16 16zM304 192l416 0 0 640-416 0 0-640zM496 928c0 17.664 14.336 32 32 32s32-14.336 32-32c0-17.664-14.336-32-32-32-17.664 0-32 14.336-32 32zM448 128l128 0c8.832 0 16-7.168 16-16s-7.168-16-16-16l-128 0c-8.832 0-16 7.168-16 16s7.168 16 16 16z"
+                                        ></path>
                                     </svg>
                                 </span>
                             </a>
@@ -88,22 +90,22 @@
                         <div class="section-bd">
                             <div class="item">
                                 <header class="item-hd">
-                                    <h3 class="item-name">天职工程咨询股份有限公司 (2017.07 至今)</h3>
+                                    <h3 class="item-name">北京中航建研航空设计咨询有限公司（2018.11 - 2019.04）</h3>
+                                    <!-- <span class="item-time">2016.09 - 2017.04</span> -->
+                                    <span class="item-time">咨询评审</span>
+                                </header>
+                                <div class="item-bd">
+                                    主要从事组织开展机场前期项目评审，如选址评审、总体规划评估、可研评估，并完成评审/评估报告等工作。
+                                </div>
+                            </div>
+                            <div class="item">
+                                <header class="item-hd">
+                                    <h3 class="item-name">天职工程咨询股份有限公司 (2017.07 -2018.07)</h3>
                                     <!-- <span class="item-time">2017.07 至今</span> -->
                                     <span class="item-time">管理审计</span>
                                 </header>
                                 <div class="item-bd">
                                     主要从事工程管理审计、招采专项检查、项目群检查、项目后评价等工作。
-                                </div>
-                            </div>
-                            <div class="item">
-                                <header class="item-hd">
-                                    <h3 class="item-name">河南普华建筑设计院（2016.09 - 2017.04 实习）</h3>
-                                    <!-- <span class="item-time">2016.09 - 2017.04</span> -->
-                                    <span class="item-time">结构工程师助理</span>
-                                </header>
-                                <div class="item-bd">
-                                    主要从事建筑结构施工图纸的绘制与修订，解决施工方的技术疑问，参与甲方需求研讨会等。
                                 </div>
                             </div>
                         </div>
@@ -117,6 +119,17 @@
                         <div class="section-bd">
                             <div class="item">
                                 <header class="item-hd">
+                                    <span>山西大同云冈机场可研评估</span>
+                                </header>
+                                <div class="item-bd">
+                                    <ul>
+                                        <li>担任改项目负责人，组织评估会工作，编写可研评估报告等。</li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div class="item">
+                                <header class="item-hd">
                                     <span>北汽福田汽车股份有限公司建设项目工程管理及效益审计</span>
                                 </header>
                                 <div class="item-bd">
@@ -125,13 +138,16 @@
                                     </ul>
                                 </div>
                             </div>
+
                             <div class="item">
                                 <header class="item-hd">
                                     <span>华夏幸福基业2017年项目营销及规划类招采管理检查</span>
                                 </header>
                                 <div class="item-bd">
                                     <ul>
-                                        <li>代表华夏幸福基业总部对各大区的采购项目进行招采检查，主要检查招标采购的合规性、合理性及管控体系的有效性。</li>
+                                        <li>
+                                            代表华夏幸福基业总部对各大区的采购项目进行招采检查，主要检查招标采购的合规性、合理性及管控体系的有效性。
+                                        </li>
                                         <li>担任该项目检查人员，负责招标采购检查等。</li>
                                     </ul>
                                 </div>
@@ -143,7 +159,9 @@
                                 </header>
                                 <div class="item-bd">
                                     <ul>
-                                        <li>对北京粮食集团各项目公司2016-2017年度发生的项目从内部决策、项目立项、勘察设计、招标采购、合同管理、施工管理、竣工验收、档案管理八个方面进行检查。</li>
+                                        <li>
+                                            对北京粮食集团各项目公司2016-2017年度发生的项目从内部决策、项目立项、勘察设计、招标采购、合同管理、施工管理、竣工验收、档案管理八个方面进行检查。
+                                        </li>
                                         <li>担任该项目的检查人员，对项目的全过程进行审计。</li>
                                     </ul>
                                 </div>
@@ -154,7 +172,9 @@
                                 </header>
                                 <div class="item-bd">
                                     <ul>
-                                        <li>对中粮置业有限公司安定门部分项目进行招采检查，主要检查项目招标采购的合规性与合理性。</li>
+                                        <li>
+                                            对中粮置业有限公司安定门部分项目进行招采检查，主要检查项目招标采购的合规性与合理性。
+                                        </li>
                                         <li>参与本项目招采部分检查，对项目采购的全过程进行审计。</li>
                                     </ul>
                                 </div>
@@ -170,39 +190,28 @@
                                     </ul>
                                 </div>
                             </div>
-
-                            <!-- <div class="item">
-                                <header class="item-hd">
-                                    <a class="btn item-more" href="https://golang.org">Golang</a>
-                                </header>
-                                <div class="item-bd">
-                                    <ul>
-                                        <li>
-                                            纯go写的直播服务端
-                                        </li>
-                                        <li>
-                                            简约的HTTP框架
-                                        </li>
-                                        <li>
-                                            高性能轻量级socks5网络混淆代理，可翻越GFW
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div> -->
                         </div>
                     </section>
-
                 </div>
 
                 <div class="content-right">
                     <section class="skill">
-
                         <!-- <header class="section-hd"> -->
                         <!-- <span class="section-title-l"></span> -->
                         <!-- <h2 class="section-title"></h2> -->
                         <!-- <span class="section-title-r"></span> -->
                         <!-- </header> -->
                         <div class="section-bd">
+                            <div class="item">
+                                <header class="item-hd">
+                                    <span>大连机场围界及安防系统改造工程可研评估</span>
+                                </header>
+                                <div class="item-bd">
+                                    <ul>
+                                        <li>担任改项目负责人，组织评估会工作，编写可研评估报告等。</li>
+                                    </ul>
+                                </div>
+                            </div>
                             <div class="item">
                                 <header class="item-hd">
                                     <span>中国印钞造币总公司基建项目全过程管理审计</span>
@@ -220,7 +229,9 @@
                                 </header>
                                 <div class="item-bd">
                                     <ul>
-                                        <li>对远洋供方库进行梳理与关联关系的检查，并对供应商类别进行重新定义与划分。</li>
+                                        <li>
+                                            对远洋供方库进行梳理与关联关系的检查，并对供应商类别进行重新定义与划分。
+                                        </li>
                                         <li>担任该项目检查人员。</li>
                                     </ul>
                                 </div>
@@ -242,7 +253,9 @@
                                 </header>
                                 <div class="item-bd">
                                     <ul>
-                                        <li>对阿里巴巴的基建项目进行招采检查，主要检查项目招标采购的合理性、合规性。</li>
+                                        <li>
+                                            对阿里巴巴的基建项目进行招采检查，主要检查项目招标采购的合理性、合规性。
+                                        </li>
                                         <li>担任本项目检查人员。</li>
                                     </ul>
                                 </div>
@@ -257,7 +270,6 @@
                             <span class="section-title-r"></span>
                         </header>
                         <div class="section-bd">
-
                             <div class="item">
                                 <div class="item-bd">
                                     <header class="item-hd">
@@ -267,7 +279,9 @@
                                         <li>熟练掌握office办公软件。</li>
                                         <li>熟悉房地产开发项目的建设程序、管理重点。</li>
                                         <li>具备多项目管理审计实施经验，熟悉国家的相关法律法规。</li>
-                                        <li>对工程建设整个过程中的招投标、工程建设管理、质量管理等具有丰富的管理和实践经验。</li>
+                                        <li>
+                                            对工程建设整个过程中的招投标、工程建设管理、质量管理等具有丰富的管理和实践经验。
+                                        </li>
                                     </ul>
                                 </div>
                             </div>
@@ -280,7 +294,6 @@
                             <span class="section-title-r"></span>
                         </header>
                         <div class="section-bd">
-
                             <div class="item">
                                 <div class="item-bd">
                                     <header class="item-hd">
